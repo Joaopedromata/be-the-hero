43fdb8f07bb85a722c16a26042d9bf62c11e1a0b
joaopedromata <joaopmata182@gmail.com>
2020-04-10 21:34:04 -0300

    front-end error

---
+++ frontend/src/pages/NewIncident/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import './style.css';
+import logoImg from '../../assets/logo.svg';
+import {Link} from 'react-router-dom';
+import { FiArrowLeft} from 'react-icons/fi';
+
+
+
+export default function NewIncident(){
+    return (
+        <div className = "new-incident-container">
+                <div className = "content">
+                    <section>
+                    <img src={logoImg} alt="Be The Hero"/>
+                    <h1>Cadastrar novo caso</h1>
+                    <p>Descreva o case detalhadamente para encontrar um héroi para resolver isso.</p>
+                    <Link className="back-link" to="/profile">
+                     <FiArrowLeft size={16} color= "#E02041"/>
+                     Voltar para home
+                    </Link>
+                    
+                    
+                    </section>
+
+                    <form>
+                        <input placeholder="Título do caso"/>
+                        <textarea placeholder="Descrição"/>
+                        <input placeholder = "Valor em reais"/>
+                        
+                        
+                        <button className="button" type= "submit">Cadastrar</button>
+
+                    </form>
+                </div>
+
+
+            </div>
+
+
+
+
+
+    );
+
+}
